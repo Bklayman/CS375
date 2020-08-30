89073e836877c096e13d6a210ec6d7a91afc3493
Brendan Klayman <bklayma1@binghamton.edu>
2020-08-30 15:54:04 -0400

    Finished function to get market price info in project 1

--- Projects/prj1/program1.cc
+++ Projects/prj1/program1.cc
@@ -1,41 +1,62 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
+
+int stringToInt(std::string originalString){
+  int result;
+  try{
+    result = std::stoi(originalString);
+  } catch(...){
+    std::cout << "Error: " << originalString << " is not an integer." << std::endl;
+    exit(1);
+  }
+  return result;
+}
+
+std::map<std::string, int> getMarketPricesMap(char* marketFileName){
+  std::fstream marketFile;
+  //Opens the market-price-file
+  marketFile.open(marketFileName);
+  if(!marketFile.is_open()){
+    std::cout << "Error: " << marketFileName << " does not exist." << std::endl;
+    exit(1);
+  }
+  std::string numMarketPricesStr;
+  if(!getline(marketFile, numMarketPricesStr)){
+    std::cout << "Error: " << marketFileName << " does not have a correct first line." << std::endl;
+  }
+  int numMarketPrices;
+  numMarketPrices = stringToInt(numMarketPricesStr);
+  std::map<std::string, int> marketPriceMap;
+  for(int i = 0; i < numMarketPrices; i++){
+    std::string marketPriceLine;
+    if(!std::getline(marketFile, marketPriceLine)){
+      std::cout << "Error: " << marketFileName << "'s line " << i << " does not exist." << std::endl;
+      exit(1);
+    }
+    int spaceIndex = marketPriceLine.find(" ");
+    if(spaceIndex == -1){
+      std::cout << "Error: Line " << i + 1 << " in " << marketFileName << " is formatted incorrectly." << std::endl;
+      exit(1);
+    }
+    std::string cardName = marketPriceLine.substr(0, spaceIndex);
+    std::string cardPriceStr = marketPriceLine.substr(spaceIndex + 1, marketPriceLine.length() - spaceIndex - 1);
+    int cardPrice = stringToInt(cardPriceStr);
+    marketPriceMap.insert(std::pair<std::string, int>(cardName, cardPrice));
+  }
+  return marketPriceMap;
+}
 
 int main(int argc, char* argv[]){
   if(argc != 5){
     std::cout << "Usage: ./program1 -m <market-price-file> -p <price-list-file>" << std::endl;
     exit(1);
   }
-  //Creates the files for market-price-file and price-list-file
-  std::fstream marketFile, listFile;
-  //Opens the market-price-file
-  marketFile.open(argv[2]);
-  if(!marketFile.is_open()){
-    std::cout << "Error: " << argv[2] << " does not exist." << std::endl;
-    exit(1);
-  }
-  //Reads market-price-file and creates a dictionary for all the names (key) and prices (value)
-  std::string numMarketPricesStr;
-  if(!getline(marketFile, numMarketPricesStr)){
-    std::cout << "Error: " << argv[2] << " does not have a correct first line." << std::endl;
-  }
-  int numMarketPrices;
-  try{
-    numMarketPrices = std::stoi(numMarketPricesStr);
-  } catch(...){
-    std::cout << "Error: " << argv[2] << " does not have a correct first line." << std::endl;
-    exit(1);
-  }
-  for(int i = 0; i < numMarketPrices; i++){
-    std::string marketPrice;
-    if(!getLine(marketFile, marketPrice)){
-      std::cout << "Error: " << argv[2] << "'s line " << i << " does not exist." << std::endl;
-      exit(1);
-    }
-    
-  }
-  //Opens the price-list-file
+
+  std::fstream listFile;
+  std::map<std::string, int> marketPricesMap = getMarketPricesMap(argv[2]);
+  
   listFile.open(argv[4]);
   if(!listFile.is_open()){
     std::cout << "Error: " << argv[4] << " does not exist." << std::endl;
